de2e151f2b6f42ea0ffa0b900a7d4f09186f597d
MugRuncherMaps <MugRuncherMaps@gmail.com>
2018-03-07 22:19:58 -0500

    update js

--- js/test.js
+++ js/test.js
@@ -3,13 +3,12 @@ console.log("Test script triggered");
 //new shit goes here
 var xhr = new XMLHttpRequest();
 xhr.onload = function() {
-  dump(xhr.responseXML.documentElement.nodeName);
+	console.log(xhr.responseURL);
+	dump(xhr.responseXML.documentElement.nodeName);
 }
 xhr.onerror = function() {
   dump("Error while getting XML.");
 }
 xhr.open("GET", "https://www.aljazeera.com/xml/rss/all.xml");
 xhr.responseType = "document";
-//var xhrHeaders = xhr.getAllResponseHeaders();
 xhr.send();
-console.log("test: \n" + xhr.getAllResponseHeaders());
